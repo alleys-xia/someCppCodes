fd50c8d8db4fe0f27a2d0b0f00e5f6cb410ef15f
xiateng8 <xiatengcs@gmail.com>
2025-06-12 13:41:41 +0800

    add stdarray examples

---
+++ stdArrayExample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+int main() {
+	std::array<int,5> myArray= {10,20,30,40,50};
+	//获取大小
+	std::cout<<"Size:"<<myArray.size()<<std::endl;
+	try{
+		std::cout<<myArray.at(2)<<std::endl;
+		std::cout<<myArray.at(10)<<std::endl;
+	}catch(std::out_of_range& e) {
+		std::cerr<<"Error: "<<e.what()<<std::endl;
+	}
+	return 0;
+}
